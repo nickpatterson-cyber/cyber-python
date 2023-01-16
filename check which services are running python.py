8f7dc2b98cfc9005c40fac3eca303226c372a0df
Nick Patterson <117977892+nickpatterson-cyber@users.noreply.github.com>
2023-01-16 21:07:16 +1100

    Update check which services are running python.py

--- check which services are running python.py
+++ check which services are running python.py
@@ -1,24 +1,21 @@
-"""
-This script uses the psutil.process_iter() function to iterate through all running processes on the machine.
-It filters the processes that are running under the svchost.exe process, as most of the services run under this process.
-It then appends all the services running under svchost.exe to a list and prints it out at the end.
-
-Please note that this script will only work on Windows and it uses the as_dict method which is only available in version 5.4.0 and higher of psutil.
-Also, the script relies on the name of the process, so if the service is running under a different process name or if the service is not running under svchost.exe it will not show up in the list.
-"""
+# This script uses the psutil.process_iter() function to iterate through all running processes on a windows system.
+# It will analyse the processes that are running under the svchost.exe process, and print it out at the end.
+# This script is for Windows only and the as.dict method only works in Python 5.4.0
+# v1.0 Dr. Nick Patterson
 
 import psutil
 
 services = []
 for process in psutil.process_iter():
     try:
+        #gather the processes - process id, name and time created
         pinfo = process.as_dict(attrs=['pid', 'name', 'create_time'])
         if pinfo['name'] == 'svchost.exe':
             for i in process.children(recursive=True):
                 services.append(i.name())
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         pass
-
+#print out which ones are running
 print("Services running:")
 for service in services:
     print(service)
