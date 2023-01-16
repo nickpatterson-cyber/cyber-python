1814789f6987f7bb9d0591b649438602949f570a
Nick Patterson <117977892+nickpatterson-cyber@users.noreply.github.com>
2023-01-16 21:37:21 +1100

    Update require users to change password linux python.py

--- require users to change password linux python.py
+++ require users to change password linux python.py
@@ -1,6 +1,8 @@
 # This script is if you suspect your passwd/shadow file have been compromised and its time to reset everyones passwords on the system!
 # This script will use the command - chage. What is chage? description below:
-# "One of the helpful tools in Linux is the “chage” command. From its name, the “chage” command is derived from the words “Change Age”, which is used to modify the information such as duration when to change password, make account status active or inactive, sets expiry date of the account, and sets a reminder to change the password through an alarm before user’s account will be inactive." - source: https://linuxhint.com/use-linux-chage-command/
+# "One of the helpful tools in Linux is the “chage” command. From its name, the “chage” command is derived from the words “Change Age”
+# - which is used to modify the information such as duration when to change password, make account status active or inactive, sets expiry date of the account
+# - and sets a reminder to change the password through an alarm before user’s account will be inactive." - source: https://linuxhint.com/use-linux-chage-command/
 # chage is used here to change the password aging info of all users on a linux system.
 # It sets the number of days since January 1st, 1970 that the users password was last changed to a factor of 0. Ultimately forcing them to change it when they next login.
 # Warning: it will do it for the root account also - so be careful using this
