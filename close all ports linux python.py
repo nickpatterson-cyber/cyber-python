4af761601b72c81ff44488f39aaf5a22ade37a1f
Nick Patterson <117977892+nickpatterson-cyber@users.noreply.github.com>
2023-01-16 21:11:38 +1100

    Update close all ports linux python.py

--- close all ports linux python.py
+++ close all ports linux python.py
@@ -1,12 +1,8 @@
-"""
-This script will execute shell command lsof -i -n -P | grep LISTEN | awk '{print $2}' | xargs kill -9 using os.system().
-This command will list all the processes that are listening on a network port on the system and then kills them using the kill -9 command.
-
-It is worth noting that this script will kill all the processes that are listening on a port, regardless of whether they are important system processes or not.
-It is also worth noting that using kill -9 will forcefully close the processes, which may cause data loss or corruption.
-
-It is highly recommended to use this script with caution. It is also recommended to check the processes that are listening on a port before closing them, and close only those that are not essential for the system to function properly.
-"""
+# This python script is used in emergencies if you anticipate a hacker is in the system and you want to kill off any open doors immediately.
+# This script will utilise the lsof command - the lsof command stands for LiSt Open Files and shows open files and which process uses them.
+# It will list all processes thet are listening on a open port and then kill them off using the kill -9 command.
+# Be careful with this one as it will kill off all listening ports without giving you a chance to finish off any transfers etc.
+# V1.0 NP
 
 import os
 
