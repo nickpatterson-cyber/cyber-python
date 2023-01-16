a0486b516d4ddd619fe398397c607044af751628
Nick Patterson <117977892+nickpatterson-cyber@users.noreply.github.com>
2023-01-16 21:34:39 +1100

    Update close all ports linux python.py

--- close all ports linux python.py
+++ close all ports linux python.py
@@ -1,7 +1,7 @@
 # This python script is used in emergencies if you anticipate a hacker is in the system and you want to kill off any open doors immediately.
-# This script will utilise the lsof command - the lsof command stands for LiSt Open Files and shows open files and which process uses them.
-# It will list all processes thet are listening on a open port and then kill them off using the kill -9 command.
-# Be careful with this one as it will kill off all listening ports without giving you a chance to finish off any transfers etc.
+# lsof command descrption: "The lsof (list open files) command returns the user processes that are actively using a file system. " -source: https://www.ibm.com/docs/en/spectrum-scale/5.1.0?topic=commands-lsof-command
+# It will list all processes thet are listening on a open port and then kill them off using the kill -9 command (the kill command is used to kill processes in linux and -9 will forcefully kill off the process without question).
+# Be careful with this one as it will kill off all listening ports without giving you a chance to finish off any transfers or activities in action etc.
 # V1.0 NP
 
 import os
